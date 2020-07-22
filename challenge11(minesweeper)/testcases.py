6916ac8e9082c9b0232a89a29d32b7624d71f633
RDornodon <51084068+RDornodon@users.noreply.github.com>
2020-07-22 11:02:50 +0200

    cleanup
    cleanup and pretty print

--- challenge11(minesweeper)/testcases.py
+++ challenge11(minesweeper)/testcases.py
@@ -1,33 +1,53 @@
 import random
 
 
-def gen(amount=50000):
+def gen(amount=50000, *, mine_ratio=0.33, max_w=20, max_h=20):
     testcases = []
-    for o in range(amount):  # amount of test cases
-        print(f"generating cases {o + 1}/{amount}", end="\r")
-        case = []
-        width = random.randint(0,20)
-        height = random.randint(0,20)
-        for h in range(height):
-            case.append([])
-            for w in range(width):
-                case[h].append(0)
-        percentage = random.randint(1,70)
-        mines_amount = int(width*height/100*percentage)
-        for i in range(mines_amount):
-            x = random.randrange(0,len(case[0]))
-            y = random.randrange(0,len(case))
-            if case[y][x] == 'x':
-                while case[y][x] == 'x':
-                    x = random.randrange(0, len(case[0]))
-                    y = random.randrange(0, len(case))
-            case[y][x] = 'x'
-        testcases.append(case)
+    for case in range(amount):
+        w = random.randint(0, max_w)
+        h = random.randint(1, max_h)
+        field = []
+        for iy in range(h):
+            row = []
+            for ix in range(w):
+                if random.random() < mine_ratio:
+                    row.append('x')
+                else:
+                    row.append(0)
+            field.append(row)
+        testcases.append(field)
     return testcases
 
 
+def gen_all(*, mine_ratio=0.33, max_w=20, max_h=20):
+    testcases = []
+    for h in range(1, max_h):
+        for w in range(max_w):
+            field = []
+            for iy in range(h):
+                row = []
+                for ix in range(w):
+                    if random.random() < mine_ratio:
+                        row.append('x')
+                    else:
+                        row.append(0)
+                field.append(row)
+            testcases.append(field)
+    return testcases
+
+
+def prettyprint(field):
+    for row in field:
+        print(''.join(map(str, row)))
+
+
 if __name__ == "__main__":
-    cases = gen(3)
+    cases = gen_all(mine_ratio=random.random()*0.67)
     for case in cases:
-        print(case)
-    #print(cases)#, [__import__("check").matrixSum(i) for i in cases])
+        try:
+            prettyprint(case)
+            #print('^'*len(case[0]))
+            #prettyprint(solution_f(case))
+            print('-'*len(case[0]))
+        except:
+            print(case)
